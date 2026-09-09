2824d8bd1f2e723b69e2312b9f5a5bf654c7204c
redsun352 <greedyy3843@gmail.com>
2026-09-09 18:55:01 +0300

    chore: remove legacy Expo app routes

--- app/_layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-
-export default function Layout() {
-  return <>
-    <StatusBar style="light" />
-    <Stack screenOptions={{ headerStyle: { backgroundColor: '#071a1b' }, headerTintColor: '#e9ffff', contentStyle: { backgroundColor: '#071214' } }} />
-  </>;
-}
